d75bc318bd8debb7ddba578130a63dbcbd63c6c2
jt-bama <43825708+jt-bama@users.noreply.github.com>
2019-03-27 15:47:23 -0500

    Allowing for all errors in Smoothing
    User errors in smoothing input no longer matter, works around them.

--- MimsENDOR.py
+++ MimsENDOR.py
@@ -117,65 +117,67 @@ class MimsENDOR(Spec_Data):
             No space accepted in input.
             Default (4,15)""",
                                 parent=application_window)
-            
-            try:
-                order = int(order_pts[0]) 
-                if order > 6:
-                    order = 6
-                    application_window = tk.Tk()
-                    application_window.withdraw()
-                    simpledialog.messagebox.showerror("Polynomial Error",
-                                                  """Invalid Sav-Gol Polynomial Order input detected. 
-            Max (6) selected""",
-                                                  parent = application_window)
-                pts = int(order_pts[1])
-                
-                if (pts/2) - np.round((pts / 2)) > 0:
-                    pts = pts
-                else:
-                    pts = pts -1
-                    application_window = tk.Tk()
-                    application_window.withdraw()
-                    simpledialog.messagebox.showerror("Point Error",
-                                                  """Invalid Sav-Gol Point input detected. 
-            Rounded down to next odd number""",
-                                                  parent = application_window)
+            if order_pts is None:
+                smoothed = self.deglitched
+            else:
+                try:
+                    order = int(order_pts[0]) 
+                    if order > 6:
+                        order = 6
+                        application_window = tk.Tk()
+                        application_window.withdraw()
+                        simpledialog.messagebox.showerror("Polynomial Error",
+                                                      """Invalid Sav-Gol Polynomial Order input detected. 
+                Max (6) selected""",
+                                                      parent = application_window)
+                    pts = int(order_pts[1])
                     
-                if pts > 49:
-                    pts = 49
+                    if (pts/2) - np.round((pts / 2)) > 0:
+                        pts = pts
+                    else:
+                        pts = pts -1
+                        application_window = tk.Tk()
+                        application_window.withdraw()
+                        simpledialog.messagebox.showerror("Point Error",
+                                                      """Invalid Sav-Gol Point input detected. 
+                Rounded down to next odd number""",
+                                                      parent = application_window)
+                        
+                    if pts > 49:
+                        pts = 49
+                        application_window = tk.Tk()
+                        application_window.withdraw()
+                        simpledialog.messagebox.showerror("Point Error",
+                                                      """Invalid Sav-Gol Point input detected. 
+                Max (49) selected""",
+                                                      parent = application_window)
+                    
+                except ValueError:
+                    order_pts = [4,15]
                     application_window = tk.Tk()
                     application_window.withdraw()
-                    simpledialog.messagebox.showerror("Point Error",
-                                                  """Invalid Sav-Gol Point input detected. 
-            Max (49) selected""",
-                                                  parent = application_window)
+                    
+                    simpledialog.messagebox.showerror("ValueError",
+                                                      """Invalid Sav-Gol parameter input detected.
+                No space accepted in input.
+                Defualt (4,15) selected""",
+                                                      parent = application_window)
+                    
+                except IndexError:
+                    order_pts = [4,15]
+                    application_window = tk.Tk()
+                    application_window.withdraw()
+                    
+                    simpledialog.messagebox.showerror("IndexError",
+                                                      """Invalid Sav-Gol parameter input detected.
+                Defualt (4,15) selected""",
+                                                      parent = application_window)            
+                    
+                order = order_pts[0]
+                pts = order_pts[1]
                 
-            except ValueError:
-                order_pts = [4,15]
-                application_window = tk.Tk()
-                application_window.withdraw()
                 
-                simpledialog.messagebox.showerror("ValueError",
-                                                  """Invalid Sav-Gol TEST parameter input detected.
-            No space accepted in input.
-            Defualt (4,15) selected""",
-                                                  parent = application_window)
-                
-            except IndexError:
-                order_pts = [4,15]
-                application_window = tk.Tk()
-                application_window.withdraw()
-                
-                simpledialog.messagebox.showerror("IndexError",
-                                                  """Invalid Sav-Gol parameter input detected.
-            Defualt (4,15) selected""",
-                                                  parent = application_window)            
-                
-            order = order_pts[0]
-            pts = order_pts[1]
-            
-            
-            smoothed = savgol_filter(self.deglitched, pts, order)
+                smoothed = savgol_filter(self.deglitched, pts, order)
 
             return smoothed
 ##############################################################################
